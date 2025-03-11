c25b18e50c3e623b6eefd539e1529ac009d95ca8
Ramyashree R <124486888+ramyashree8470@users.noreply.github.com>
2025-03-11 15:39:31 +0530

    Update dev.java

--- dev.java
+++ dev.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 class dev {
     public static void main(String[] args)
     {
-        System.out.prinln("Hello World");
+        System.out.println("Hello World");
     }
 }
